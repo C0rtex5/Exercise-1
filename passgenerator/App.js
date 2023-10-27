8d2a1a4e851827d53b5a4e791a8dc5861eb8d81a
Abhay <4bh4y.m3nd3s@gmail.com>
2023-10-27 12:42:25 +0100

    update

--- passgenerator/App.js
+++ passgenerator/App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text, StyleSheet, Image } from 'react-native' 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+export default function App(){
+  return(
+    <View style={ styles.container }>
+      <Image
+      source={require("./src/assets/logo.png")}
+      sytyles={styles.logo}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+  container:{
+    flex:1,
+    backgroundColor: "#F3F3FF",
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  logo:{
+    marginBottom: 60
+  }
+})
